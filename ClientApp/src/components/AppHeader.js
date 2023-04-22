93b9b0658fef2f901fd8cb370578ae1177664038
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-04-22 20:16:13 +0200

    small changes in html

--- ClientApp/src/components/AppHeader.js
+++ ClientApp/src/components/AppHeader.js
@@ -6,20 +6,23 @@ export default class AppHeader extends Component {
 
     render() {
         return (
-            <div>
+            <div >
                 <Navbar>
-                    <Nav className="mx-auto" >
-                    <NavbarBrand>HR PLATFORM</NavbarBrand>
-                        <NavItem>
-                            <NavLink onClick={this.props.candidateClick} href="/" >Job Candidates</NavLink>
-                    </NavItem>
-                        <NavItem>
-                            <NavLink onClick={this.props.skillClick} href="/">Skills</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/">About</NavLink>
-                    </NavItem>
-                </Nav>
+                    <Nav style={{ width: '100%' }}>
+                        <NavbarBrand >HR PLATFORM</NavbarBrand>
+                        
+                            <NavItem>
+                                <NavLink onClick={this.props.candidateClick} href="/" >Job Candidates</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink onClick={this.props.skillClick} href="/">Skills</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">About</NavLink>
+                            </NavItem>
+                        
+                        
+                    </Nav>
                 </Navbar>
             </div>
         )
